6a0ad890cf9b792d97c45c3ed729d7f0d26d6e7f
Romy Chu II <36515813+RomyIChuII@users.noreply.github.com>
2018-02-22 10:56:12 -0500

    Initial commit

---
+++ AdressProgramRomyIIC.cs/frmAddressProgram.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace AdressProgramRomyIIC.cs
+{
+    partial class frmAddressProgramRomyIIC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Monospac821 BT", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(116, 36);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(454, 39);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "ADDRESS PROGRAM ROMYIIC";
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Font = new System.Drawing.Font("Monospac821 BT", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLocation.Location = new System.Drawing.Point(26, 115);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(678, 35);
+            this.lblLocation.TabIndex = 1;
+            this.lblLocation.Text = "Chinese Embassy Ottawa, Ontario, Canada";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monospac821 BT", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(644, 35);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "515 St Patrick St, Ottawa, ON K1N 5H3";
+            // 
+            // frmAddressProgramRomyIIC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(707, 317);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblLocation);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmAddressProgramRomyIIC";
+            this.Text = "AddressProgram Romy I. Chu II";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AdressProgramRomyIIC.cs/frmAddressProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdressProgramRomyIIC.cs
+{
+    public partial class frmAddressProgramRomyIIC : Form
+    {
+        public frmAddressProgramRomyIIC()
+        {
+            InitializeComponent();
+        }
+    }
+}
